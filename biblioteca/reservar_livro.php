841ac7c10ec4269d7eae09d7bd50024c1a0f9e08
furtado <rafaelfurt28@gmail.com>
2023-06-14 11:19:51 -0300

    Terminado 'reservar_livro.php' e 'cadastrar_reserva.php'

--- biblioteca/reservar_livro.php
+++ biblioteca/reservar_livro.php
@@ -1,71 +1,42 @@
-1 <!DOCTYPE html>
-2
+<!DOCTYPE html>
 <html lang="pt">
-3 <head>
-4
-<meta charset="UTF-8">
-5 6
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-71 <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
-8
-<title>Reservar Livro</title>
-9 10 <body>
--</head>
-11
-<h1> Reserve seu Livro </h1>
-12
-<form method="post" action="cadastrar_reserva.php">
-13
-<label> Nome Usuário </label>
-14
-<input type="text" name="nome_usuario">
-15
-<label> Data Reserva </label>
-16
-<input type="date" name="data_reserva">
-17
-<label for="Livro"> Selecione o Livro: </label>
-18
-<
-select id="Livro" name="livro">
-19
-<option value=""> Selecione um Livro </option>
-20 ~
-<?php
-21 ~
-$host 'localhost';
-22
-$usuario 'root';
-23 ~
-$senha '';
-24
-$banco 'biblioteca'; =
-25 ~
-26 27 ~~
-$conexao mysqli_connect ($host, $usuario, $senha, $banco); =
-28 ~
-$sql = "select id, titulo from livro"; $resultado = mysqli_query($conexao, $sql);
-29
-30 mm
-31
-while ($row mysqli_fetch_assoc($resultado)) { =
-32 mm
-echo "<option value= ".$row["id"].">".$row["titulo"]."</option>"; }
-33
-34 mm
-35
-mysqli_close($conexao);
-36
-?>
-37 mm
-38
-</select> <br> <br>
-39 mm
-<input type="submit" value="Cadastrar">
-40
-</form>
-41 44
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <title>Reservar Livro</title>
+</head>
+<body>
+    <h1> Reserve seu Livro </h1>
+    <form method="post" action="cadastrar_reserva.php">
+        <label> Nome Usuário </label>
+        <input type="text" name="nome_usuario">
+        <label> Data Reserva </label>
+        <input type="date" name="data_reserva">
+        <label for="Livro"> Selecione o Livro: </label>
+        <select id="Livro" name="livro">
+            <option value=""> Selecione um Livro </option>
+            <?php
+                $host 'localhost';
+                $usuario 'root';
+                $senha '';
+                $banco 'biblioteca'; 
+
+                $conexao mysqli_connect ($host, $usuario, $senha, $banco); 
+                
+                $sql = "select id, titulo from livro"; 
+                $resultado = mysqli_query($conexao, $sql);
+                
+                while ($row = mysqli_fetch_assoc($resultado)) { 
+                    echo "<option value= ".$row["id"].">".$row["titulo"]."</option>"; 
+                }
+                
+                mysqli_close($conexao);
+            ?>
+
+        </select> <br> <br>
+        <input type="submit" value="Cadastrar">
+    </form>
 </body>
-42
--</html>
+</html>
